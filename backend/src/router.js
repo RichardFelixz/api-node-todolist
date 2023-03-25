dae0cedd4b9a1a390ef306e197ad619f71ac77be
Richard <richard@datamob.com.br>
2023-03-25 12:20:55 -0300

    First commit

---
+++ backend/src/router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const tasksController = require('./controllers/tasksController')
+const tasksMiddleware = require('./middlewares/tasksMiddleware');
+
+const router = express.Router();
+
+router.get('/tasks', tasksController.getAll);
+router.post('/tasks', tasksMiddleware.validateFieldTitle , tasksController.createTask);
+router.delete('/tasks/:id', tasksController.deleteTask);
+router.put('/tasks/:id', 
+tasksMiddleware.validateFieldTitle, 
+tasksMiddleware.validateFieldStatus, 
+tasksController.updateTask);
+
+module.exports = router;
